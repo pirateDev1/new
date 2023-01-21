c6e6fec4363c0c8667bab7b8a78380426965a816
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-01-21 21:04:33 +0300

    intermediate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { FirstScreen } from "modules/main/components/FirstScreen"
+import { Button } from "modules/common/ui/Button"
 
 function App() {
     return (
         <div>
             <FirstScreen />
+            <Button>4len</Button>
         </div>
     )
 }

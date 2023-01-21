f9c7e0036777efc5987975ad6cfedcac51c7e951
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-01-21 21:31:31 +0300

    add button ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Main } from "modules/main/components/Main"
 import React from "react"
-import { Button } from "modules/common/ui/Button"
 
 function App() {
     return (
         <div>
             <Main />
-            <Button>4len</Button>
         </div>
     )
 }

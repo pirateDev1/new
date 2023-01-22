24e55fba6f0bf7b70d25fd6239cd91f094f6d98c
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-01-22 21:08:16 +0300

    finished thank you modal

--- src/modules/GratitudeModal/GratitudeModal.jsx
+++ src/modules/GratitudeModal/GratitudeModal.jsx
@@ -1,6 +1,7 @@
 import { AnimatedModal } from "modules/common/ui/AnimatedModal"
 import React from "react"
 import styles from "./GratitudeModal.module.css"
+import { RxCross1 } from "react-icons/rx"
 
 function GratitudeModal({ showGratitudeModal, setShowGratitudeModal }) {
     return (
@@ -9,6 +10,10 @@ function GratitudeModal({ showGratitudeModal, setShowGratitudeModal }) {
             onClose={() => setShowGratitudeModal(false)}
             innerClassName={styles.container}
         >
+            <RxCross1
+                className={styles.close}
+                onClick={() => setShowGratitudeModal(false)}
+            />
             <p className={styles.info}>
                 Ваша заявка принята! С Вами свяжутся в ближайшее время! <br />
                 Спасибо за Ваше обоащение!

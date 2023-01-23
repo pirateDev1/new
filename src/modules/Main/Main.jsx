fadba2945f75cfe4d89dec54b7e82421a2cbbc18
pirateDev1 <123244716+pirateDev1@users.noreply.github.com>
2023-01-24 02:26:58 +0300

    raw mobile layout
    to do:
    laptop layout
    reusible components
    svg for items
    delete useless png
    assets folder structure

--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -8,28 +8,28 @@ function Main() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
-                <h3 className={styles.topText}>Обучение ЦАБ</h3>
-                <div className={styles.imgContainer}>
-                    <img src={headerImg} alt="" />
-                </div>
-                <div className={styles.infoContainer}>
-                    <p className={styles.smallText}>
-                        Гибридное образование с практическим уклоном
-                    </p>
-                    <h2>
-                        Обучаем цифровым профессиям будущего
-                        <span>в сфере Web 3.0</span>
-                    </h2>
-                    <CustomLicense />
-                    <div className={styles.license}>
-                        <img
-                            src="https://static.tildacdn.com/tild3666-6137-4266-a263-366661646262/smalllogo.svg"
-                            alt=""
-                        />
-                        <p>Государственная образовательная лицензия</p>
+                <div className={styles.content}>
+                    <h3 className={styles.topText}>Обучение ЦАБ</h3>
+                    <div className={styles.imgContainer}>
+                        <img src={headerImg} alt="" />
+                    </div>
+                    <div className={styles.infoContainer}>
+                        <p className={styles.smallText}>
+                            Гибридное образование с практическим уклоном
+                        </p>
+                        <h2 className={styles.mainText}>
+                            Обучаем цифровым профессиям будущего
+                            <span>в сфере Web 3.0</span>
+                        </h2>
+                        {/* 
+                    <CustomLicense text="Государственная образовательная лицензия" /> */}
+                        <div className={styles.customIconsContainer}>
+                            <CustomLicense text="Бесплатно до конца февраля" />
+                            <CustomLicense text="Длительность курса 7 дней" />
+                        </div>
                     </div>
                 </div>
-                <Button>Записаться</Button>
+                <Button className={styles.btn}>Записаться</Button>
             </div>
         </div>
     )

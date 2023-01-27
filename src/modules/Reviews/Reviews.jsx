a3147452081b50d7596d4ec99fe3669468dddfb3
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-01-27 20:07:13 +0300

    made component "Waves"

--- src/modules/Reviews/Reviews.jsx
+++ src/modules/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { ReviewsSwiper } from "./components/ReviewsSwiper"
 import styles from "./Reviews.module.css"
+import { Waves } from "modules/common/components/Waves"
 
 function Reviews() {
     return (
@@ -9,19 +10,7 @@ function Reviews() {
                 <h2 className={styles.title}>Что о нас говорят студенты?</h2>
                 <ReviewsSwiper />
             </div>
-            <div className={styles.wave}>
-                <svg
-                    data-name="Layer 1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1200 120"
-                    preserveAspectRatio="none"
-                >
-                    <path
-                        d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-                        className={styles.shapeFill}
-                    ></path>
-                </svg>
-            </div>
+            <Waves />
         </div>
     )
 }

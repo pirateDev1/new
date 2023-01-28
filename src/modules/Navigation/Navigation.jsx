38ea52d1b85043dec633fffe87b0bb3906de54e0
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-01-28 15:20:34 +0300

    responsive header

--- src/modules/Navigation/Navigation.jsx
+++ src/modules/Navigation/Navigation.jsx
@@ -1,13 +1,18 @@
 import React from "react"
+import styles from "./Navigation.module.css"
 import { Burger } from "./components/Burger"
+import { Logo } from "./components/Logo"
 import { Navbar } from "./components/Navbar"
 
 function Navigation() {
     return (
-        <>
-            <Navbar />
-            <Burger />
-        </>
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <Logo />
+                <Navbar />
+                <Burger />
+            </div>
+        </div>
     )
 }
 
--- src/modules/Navigation/components/Navbar/Navbar.jsx
+++ src/modules/Navigation/components/Navbar/Navbar.jsx
@@ -6,7 +6,6 @@ import { NavigationList } from "../common"
 export default function Navbar() {
     return (
         <div className={styles.container}>
-            <h3 className={styles.logo}>Обучение ЦАБ</h3>
             <NavigationList />
             <CtaButton className={styles.btn}>Заказать</CtaButton>
         </div>

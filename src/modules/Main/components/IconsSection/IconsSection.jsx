c90bdc3c78e19ed7cf4261b1c8f14bc3618c239d
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-02-07 19:12:36 +0300

    style fixes

--- src/modules/Main/components/IconsSection/IconsSection.jsx
+++ src/modules/Main/components/IconsSection/IconsSection.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import styles from "./IconsSection.module.css"
 import { CustomIconDescription } from "modules/common/components/CustomIconDescription"
 import { rubleImg, timeImg } from "assets/pictures/main"
+<<<<<<< Updated upstream
 
 function IconsSection() {
   return (
@@ -10,6 +11,18 @@ function IconsSection() {
       <CustomIconDescription text="Длительность курса 7 дней" img={timeImg} />
     </div>
   )
+=======
+import { OfferMain } from "./components/OfferMain"
+import { DurationMain } from "./components/DurationMain"
+
+function IconsSection() {
+    return (
+        <div className={styles.container}>
+            <CustomIconDescription element={<OfferMain />} img={rubleImg} />
+            <CustomIconDescription element={<DurationMain />} img={timeImg} />
+        </div>
+    )
+>>>>>>> Stashed changes
 }
 
 export default IconsSection
---
+++ src/modules/Main/components/IconsSection/components/DurationMain/DurationMain.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./DurationMain.module.css"
+
+function DurationMain() {
+    return <p className={styles.text}>Длительность курса 7 дней</p>
+}
+
+export default DurationMain

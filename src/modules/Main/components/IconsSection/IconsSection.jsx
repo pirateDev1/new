3ca67a7fa9cab884d131d9d8fb11c8425ef30328
pirateDev2 <pirateDev002@outlook.com>
2023-02-07 16:43:20 -0800

    add price

--- src/modules/Main/components/IconsSection/IconsSection.jsx
+++ src/modules/Main/components/IconsSection/IconsSection.jsx
@@ -2,20 +2,15 @@ import React from "react"
 import styles from "./IconsSection.module.css"
 import { CustomIconDescription } from "modules/common/components/CustomIconDescription"
 import { rubleImg, timeImg } from "assets/pictures/main"
+import { OfferMain } from "./components"
 
 function IconsSection() {
-    return (
-        <div className={styles.container}>
-            <CustomIconDescription
-                text="Бесплатно до конца февраля"
-                img={rubleImg}
-            />
-            <CustomIconDescription
-                text="Длительность курса 7 дней"
-                img={timeImg}
-            />
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <CustomIconDescription element={<OfferMain />} img={rubleImg} />
+      <CustomIconDescription text="Длительность курса 7 дней" img={timeImg} />
+    </div>
+  )
 }
 
 export default IconsSection

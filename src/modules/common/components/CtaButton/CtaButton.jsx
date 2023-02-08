2b318c3981f5435af6fb426e818a8067eb07f1ac
pirateDev2 <123245449+pirateDev2@users.noreply.github.com>
2023-02-08 16:38:09 +0300

    Update CtaButton.jsx

--- src/modules/common/components/CtaButton/CtaButton.jsx
+++ src/modules/common/components/CtaButton/CtaButton.jsx
@@ -2,21 +2,22 @@ import { Button } from "modules/common/ui/Button"
 import React from "react"
 
 function CtaButton({ children, className, variant, onClick = null, ...props }) {
-  function clickHandler(e) {
-    const nameInput = document.getElementById("nameInput")
-    nameInput.focus()
-    onClick && onClick(e)
-  }
-  return (
-    <Button
-      variant={variant}
-      className={className}
-      props={props}
-      onClick={clickHandler}
-    >
-      {children}
-    </Button>
-  )
+    function clickHandler(e) {
+        document.querySelector("#form").scrollIntoView()
+        document.querySelector("#nameInput").focus({ preventScroll: true })
+
+        onClick && onClick(e)
+    }
+    return (
+        <Button
+            variant={variant}
+            className={className}
+            props={props}
+            onClick={clickHandler}
+        >
+            {children}
+        </Button>
+    )
 }
 
 export default CtaButton

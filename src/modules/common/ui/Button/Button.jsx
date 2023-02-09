7824a35a4d9b9b51764fc8ceda5a3a46393171d8
pirateDev2 <pirateDev002@outlook.com>
2023-02-08 16:06:51 -0800

    inter

--- src/modules/common/ui/Button/Button.jsx
+++ src/modules/common/ui/Button/Button.jsx
@@ -2,31 +2,31 @@ import React from "react"
 import styles from "./Button.module.css"
 
 export default function Button({ children, variant, className, ...props }) {
-  let btnVar = "filled"
+    let btnVar = "filled"
 
-  switch (variant) {
-    case "filled":
-      btnVar = "filled"
-      break
-    case "outlined":
-      btnVar = "outlined"
-      break
-    case "textBtn":
-      btnVar = "textBtn"
-      break
-    default:
-      btnVar = "filled"
-  }
+    switch (variant) {
+        case "filled":
+            btnVar = "filled"
+            break
+        case "outlined":
+            btnVar = "outlined"
+            break
+        case "textBtn":
+            btnVar = "textBtn"
+            break
+        default:
+            btnVar = "filled"
+    }
 
-  return (
-    <button
-      className={`${styles.btn} ${styles[btnVar]} ${className}`}
-      {...props}
-    >
-      <div className={styles.gradient}>
-        {<p className={styles.text}>{children}</p>}
-      </div>
-      {children}
-    </button>
-  )
+    return (
+        <button
+            className={`${styles.btn} ${styles[btnVar]} ${className}`}
+            {...props}
+        >
+            <div className={styles.gradient}>
+                {<p className={styles.text}>{children}</p>}
+            </div>
+            {children}
+        </button>
+    )
 }
